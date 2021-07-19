2f172d2e484ee5a87b451437ee1fa0f6406f2c59
H1ako <nikita-sobolev-wd@yandex.ru>
2021-07-19 23:38:16 +0400

    add other folders

---
+++ JS/for_ajax.js
@@ -0,0 +1,10 @@
+$('#form-contact').submit(function (event) {
+    event.preventDefault();
+    var form = $(this);
+    var form_action = form.attr('action');
+    var form_data = form.serialize();
+
+    $posting = $.post(form_action, form_data, function () {
+        // alert('posted');
+    });
+})
---
+++ JS/for_buttons.js
@@ -0,0 +1,12 @@
+const btn_hire = $('#btn-hire');
+const btn_menu = $('#btn-menu');
+btn_hire.on('click', function () {
+    console.log(document.documentElement.clientWidth);
+})
+
+
+btn_menu.on('click', function () {
+    var navigate = $('#header-nav');
+    var topBar = $('#section-header-top-bar');
+    navigate.toggleClass('collapsed');
+})
